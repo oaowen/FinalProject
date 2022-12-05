d09ccbc0bd3fc9ee9fbdb7bbea4d82393b07cc4c
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-05 13:58:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,21 @@ include('header.php');
   color: maroon;
   text-align: center;
 }
+div 1{
+  border: 1px solid black;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  margin-right: 150px;
+  margin-left: 80px;
+  background-color: lightblue;
+}
+
 </style>
     
   </head>
   <body>
     <h1 style="background-color:LightGray;">Welcome to the library!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <div 1>The library was created so that all can find a book that suits them. The library should be a place of rest and where you can expand your mind <div 1>
   </body>
 </html>
