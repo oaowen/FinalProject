4b7fef07d598029e93d38c8a79bb2fb48580f849
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-05 13:55:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ include('header.php');
     <style>
     h1 {
   color: maroon;
-  margin-left: 40px;
+  text-align: center;
 }
 </style>
     
   </head>
   <body>
-    <h1 style="background-color:LightGray;">Display 1</h1>
+    <h1 style="background-color:LightGray;">Welcome to the library!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
