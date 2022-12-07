40f73029e3cc0baff223bf26522a8d0c8de50999
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-06 20:37:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ The Library
 
 </div>
     </br>
- <div class="card" style="width: 400px; height: 300px; margin: auto; text-align: center;">
+ <div class="card" style="width: 100%; height: 300px; margin: auto; text-align: center;">
    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
