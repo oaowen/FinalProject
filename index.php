54633064d1a025caa86854ae440d839fecf4ec1f
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-06 20:51:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -47,6 +47,12 @@ The Library
   <tr>
     <td>
       <div class="card" style="width: 200px; height: 400px;">
+        <div class="card-header">
+ 
+
+<span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Featured</span>
+
+</div>
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -76,13 +82,38 @@ The Library
       </div>
     </td>
     <td>
-      <div class="card" style="width: 200px; height: 300px;">
+      <div class="card" style="width: 200px; height: 400px;">
         <img src="https://digital.library.unt.edu/ark:/67531/metadc443/m1/1/high_res/" class="img-fluid" alt="...">
       </div>
     </td>
     <td>
-      <div class="card" style="width: 200px; height: 300px;">
-        <!-- Card 3 content goes here -->
+      <div class="card" style="width: 200px; height: 400px;">
+          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" >
+    <div class="carousel-item active">
+      <img src="https://marketplace.canva.com/EAFEaEHdCUw/1/0/1131w/canva-blue-white-with-books-illustration-welcome-to-the-library-school-poster-OWKnNwm3qBc.jpg" class="d-block w-100"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.loc.gov/preservation/resources/educational/bookmarks/BKBCposters/images/spinethumb.jpg" class="d-block w-100 h-50"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.atlasobscura.com/rMAgytOPdjbglg93uJhSss1fLdcc8pIyfEL8_wBEJUI/rs:fill:12000:12000/q:81/sm:1/scp:1/ar:1/aHR0cHM6Ly9hdGxh/cy1kZXYuczMuYW1h/em9uYXdzLmNvbS91/cGxvYWRzL2Fzc2V0/cy81ZWFkOGIwNDNh/MDAxYWZlN2ZfM2Yw/NTIyOXYuanBn.jpg" class="d-block w-100"  alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
       </div>
     </td>
   </tr>
