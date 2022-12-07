4069981bb5502df9f6ed7478110872cac3ae05d6
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-06 21:36:08 -0600

    Update book.php

--- book.php
+++ book.php
@@ -73,5 +73,13 @@ $conn->close();
      </div>
      </div>
    </div>
+   <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address for book request</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Book Description</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
   </body>
 </html>
