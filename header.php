782b4ae94ce8ec6b3fdc5c4ede100e1ad5eb221f
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-07 12:50:58 -0600

    Update header.php

--- header.php
+++ header.php
@@ -20,6 +20,21 @@
         <a class="nav-link" href="author.php">Authors</a>
         <a class="nav-link" href="#">Publishers</a>
          <a class="nav-link" href="stat.html">Statistics</a>
+        <script src= "https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script src= "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+
+<body>
+<div id="root"></div>
+
+<script type="text/babel">
+function tick() {
+  const element = (<h1>{new Date().toLocaleTimeString()}</h1>);
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+</script>
    
         
       </div>
