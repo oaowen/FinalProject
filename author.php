2484b34557f55957aa01f0b8050f34f2e5fb0143
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-06 22:30:07 -0600

    Update author.php

--- author.php
+++ author.php
@@ -11,6 +11,15 @@ include('header.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
+  <div class="card">
+<div class="card-header">
+ 
+
+<span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">The Mission Statement</span>
+
+</div>
+
+<div class="card-body">
     <div class="container">
       
 <?php
@@ -71,6 +80,7 @@ $conn->close();
       </br>
       
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    
+    </div>
+    </div>
   </body>
 </html>
