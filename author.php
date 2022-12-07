49842980d2d9858786d49992c67ce648cded5cdc
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-06 22:16:44 -0600

    Update author.php

--- author.php
+++ author.php
@@ -27,6 +27,15 @@ if ($conn->connect_error) {
 }
 
 ?>
+<div class="card">
+<div class="card-header">
+ 
+
+<span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">The Mission Statement</span>
+
+</div>
+
+<div class="card-body">
     
       <h1>Authors</h1>
       <table class="table-light">
@@ -70,5 +79,8 @@ $conn->close();
       </br>
       
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </div>
+    </div>
+    </div>
   </body>
 </html>
