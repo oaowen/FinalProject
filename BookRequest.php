68b2aad458bf261be175a5e30e4ea35b53908d31
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-14 23:37:58 -0600

    Update BookRequest.php

--- BookRequest.php
+++ BookRequest.php
@@ -25,10 +25,7 @@ include('header.php');
      
     
     <?php
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Get the form data
-  $name = $_POST["bookName"];
-  $email = $_POST["email"];
+    
 $servername = "localhost";
 $username = "oaowenou_finalproject";
 $password = "TAnner01!!";
@@ -40,11 +37,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-        $sql = "INSERT INTO Request (BRName, BREmail) VALUES (?, ?)";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ss", $name, $email);
-  $stmt->execute();
-
+  
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
@@ -59,9 +52,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $stmtEdit = $conn->prepare($sqlEdit);
   $stmtEdit->bind_param("ssi", $_POST['iName'], $_POST['iEmail'], $_POST['iid']);
   $stmtEdit->execute();
-  echo '<div class="alert alert-success" role="alert
-
- 
  echo '<div class="alert alert-success" role="alert">Request edited.</div>';
   break;
 
