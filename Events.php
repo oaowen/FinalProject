7f4d7400f188d79f66de5844a3b5ff86e6c5c40d
oaowen <89744805+oaowen@users.noreply.github.com>
2022-12-15 22:33:44 -0600

    Create Events.php

---
+++ Events.php
@@ -0,0 +1,27 @@
+<?php
+include('header.php');
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+   <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://dabbledoneright-production.s3.amazonaws.com/production/uploads/course/photo/106081/carousel_writing-300x284.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Writing Classes</h5>
+        <p class="card-text">Have you ever wanted to be a writer? Do you have that book idea stuck in your head? Professor Olivia Owen will teach you how to write with grace and class. Attendees will need a laptop or paper and pencil. Class will be held in the community room at 7pm on Thursdays. Topics will range from poetry to expository wrtiting. Please bring your own refreshments and original ideas.</p>
+        <p class="card-text"><small class="text-muted">Please see librarians for more details</small></p>
+      </div>
+    </div>
+  </div>
+</div>
